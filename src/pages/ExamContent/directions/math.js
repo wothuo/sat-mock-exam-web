3b3cbff5a2105ef6ab07d937a91f3558d45825c5
Linn508 <l18428099668@163.com>
2026-02-21 13:39:53 +0800

    feat: 完成数学direction

--- src/pages/ExamContent/directions/math.js
+++ src/pages/ExamContent/directions/math.js
@@ -3,44 +3,53 @@
  */
 export const mathDirections = {
   title: 'Directions',
-  content: `The questions in this section address a number of important reading and writing skills.
-Use of a calculator is not permitted for this section. These directions can be accessed throughout the test.
-
-**For multiple-choice questions**, solve each problem and choose the correct answer from the choices provided.
-Each multiple-choice question has a single correct answer.
-
-**Mathematical Formulas Reference:**
-• Quadratic Formula: $x = \\frac{-b \\pm \\sqrt{b^2 - 4ac}}{2a}$
-• Area of a Circle: $A = \\pi r^2$
-• Pythagorean Theorem: $a^2 + b^2 = c^2$
-
-**Reference Table:**
-<table class="w-full border-collapse border border-gray-200 mt-0 mb-6 text-sm rounded-xl overflow-hidden">
-      <thead>
-        <tr class="bg-gray-50">
-      <th class="border border-gray-200 p-3 text-left font-bold text-gray-900">Property</th>
-      <th class="border border-gray-200 p-3 text-left font-bold text-gray-900">Formula / Value</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td class="border border-gray-200 p-3 text-gray-700">Slope-Intercept Form</td>
-      <td class="border border-gray-200 p-3 font-mono text-blue-600">$y = mx + b$</td>
-    </tr>
-    <tr>
-      <td class="border border-gray-200 p-3 text-gray-700">Volume of a Sphere</td>
-      <td class="border border-gray-200 p-3 font-mono text-blue-600">$V = \\frac{4}{3}\\pi r^3$</td>
-    </tr>
-    <tr>
-      <td class="border border-gray-200 p-3 text-gray-700">Constant $\\pi$</td>
-      <td class="border border-gray-200 p-3 font-mono text-blue-600">$\\approx 3.14159$</td>
-    </tr>
-  </tbody>
-</table>
-
-**For student-produced response questions:**
-• If you find more than one correct answer, enter only one answer.
-• You can enter up to 5 characters for a positive answer and up to 6 characters (including the negative sign) for a negative answer.
-• If your answer is a fraction that doesn't fit in the provided space, enter the decimal equivalent.
-• If your answer is a decimal that doesn't fit in the provided space, enter it by truncating or rounding at the fourth digit.`
+  content: `
+<div class="tet">
+  <p style="text-align: left; line-height: normal;">The questions in this section address a number of important math skills.</p>
+  <p>Use of a calculator is permitted for all questions. A reference sheet, calculator,and these directions can be accessed&nbsp;</p>
+  <p>throughout the test.</p>
+  <p>Unless otherwise indicated:</p>
+  <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style="font-size: 6px;vertical-align: middle;">●</span> All variables and expressions represent real numbers.</p>
+  <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style="font-size: 6px;vertical-align: middle;">●</span><span style="text-wrap: wrap;">&nbsp;</span>Figures provided are drawn to scale.</p>
+  <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style="font-size: 6px;vertical-align: middle;">●</span><span style="text-wrap: wrap;">&nbsp;</span>All figures lie in a plane.</p>
+  <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style="font-size: 6px;vertical-align: middle;">●</span><span style="text-wrap: wrap;">&nbsp;</span>The domain of a given function fis the set of all real numbers x for which f(<img src="https://r.latexeasy.com/image.svg?x" data-formula-image="x" class="loadingclass">)is a real number.</p>
+  <p>For <strong>multiple-choice questions,&nbsp;</strong>solve each problem and choose the correct answer from the choices provided.</p>
+  <p>Each multiple-choice question has a single correct answer.</p>
+  <p>For <strong>student-produced response questions,&nbsp;</strong>solve each problem and enter your answer as described below.</p>
+  <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style="font-size: 6px;vertical-align: middle;">●</span><span style="text-wrap: wrap;">&nbsp;</span>If you find <strong>more than one correct answer,&nbsp;</strong>enter only one answer.</p>
+  <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style="font-size: 6px;vertical-align: middle;">●</span><span style="text-wrap: wrap;">&nbsp;</span>You can enter up to 5 characters for a <strong>positive</strong> answer and up to 6 characters (including the negative sign) for a <strong>negative</strong> answer.</p>
+  <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style="font-size: 6px;vertical-align: middle;">●</span><span style="text-wrap: wrap;">&nbsp;</span>If your answer is a <strong>fraction</strong> that doesn't fit in the provided space, enter the decimal equivalent.</p>
+  <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style="font-size: 6px;vertical-align: middle;">●</span><span style="text-wrap: wrap;">&nbsp;</span>If your answer is a <strong>decimal</strong> that doesn't fit in the provided space, enter it by truncating or rounding at the fourth digit.</p>
+  <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style="font-size: 6px;vertical-align: middle;">●</span><span style="text-wrap: wrap;">&nbsp;</span>If your answer is a <strong>mixed number</strong>(such as <img src="https://r.latexeasy.com/image.svg?3%5Cfrac%20%7B1%7D%20%7B2%7D" data-formula-image="3%5Cfrac%20%7B1%7D%20%7B2%7D" width="20" height="20" border="0" vspace="0" style="width: 20px; height: 20px;">), enter it as an improper fraction(7/2)or its decimal equivalent (3.5).</p>
+  <p><span style="text-wrap: wrap;">&nbsp; &nbsp; &nbsp; &nbsp; <span style="font-size: 6px;vertical-align: middle;">●</span>&nbsp;</span>Don't enter <strong>symbols</strong> such as a percent sign, comma, or dollar sign.</p>
+  <p><br></p>
+  <p style="text-align: center;"><span style="font-size: 18px;">Examples</span></p>
+  <p><br></p>
+  <table align="center">
+    <tbody>
+      <tr class="firstRow">
+        <td width="160" valign="top" style="word-break: break-all;" align="center"><p><br></p><p style="line-height: 0.5em;">Answer</p></td>
+        <td width="160" valign="top" style="word-break: break-all;" align="center"><p>Acceptable ways to&nbsp;&nbsp;</p><p>enter answer</p></td>
+        <td width="160" valign="top" style="word-break: break-all;" align="center"><p>&nbsp;Unacceptable:will</p><p>NOT receive credit&nbsp;</p></td>
+      </tr>
+      <tr>
+        <td width="160" valign="top" style="word-break: break-all;" align="center"><p><br></p><p><br></p><p style="line-height: 0.5em;"><br></p><p><img src="https://r.latexeasy.com/image.svg?3.5" data-formula-image="3.5"></p></td>
+        <td width="160" valign="top" style="word-break: break-all;" align="center"><p><span style="background-color: #F0F0F0;"><br></span></p><p><span style="background-color: #F0F0F0;">3.5</span></p><p style="line-height: 0.5em;"><span style="background-color: #F0F0F0;"><br></span></p><p><span style="background-color: #F0F0F0;">3.50</span><br></p><p style="line-height: 0.5em;"><span style="background-color: #F0F0F0;"><br></span></p><p><span style="background-color: #F0F0F0;">7/2</span></p><p><span style="background-color: #F0F0F0;"><br></span></p></td>
+        <td width="160" valign="top" style="word-break: break-all;" align="center"><p style="line-height: 0.5em;"><br></p><p style="line-height: 0.5em;"><br></p><p style="line-height: 0.5em;"><br></p><p><span style="background-color: #F0F0F0;">31/2</span></p><p><span style="background-color: #F0F0F0;"><br></span></p><p style="line-height: 0.5em;"><span style="background-color: #F0F0F0;"><br></span></p><p><span style="background-color: #F0F0F0;">3 1/2</span><br></p><p><span style="background-color: #F0F0F0;"><br></span></p></td>
+      </tr>
+      <tr>
+        <td width="160" valign="top" style="word-break: break-all;" align="center"><p><br></p><p><br></p><p><br></p><p><img src="https://r.latexeasy.com/image.svg?%5Cfrac%20%7B2%7D%20%7B3%7D" data-formula-image="%5Cfrac%20%7B2%7D%20%7B3%7D"><br><br></p></td>
+        <td width="160" valign="top" style="word-break: break-all;" align="center"><p><span style="background-color: #F0F0F0;"><br></span></p><p><span style="background-color: #F0F0F0;">2/3</span></p><p style="line-height: 0.5em;"><span style="background-color: #F0F0F0;"><br></span></p><p><span style="background-color: #F0F0F0;">.6666</span></p><p style="line-height: 0.5em;"><span style="background-color: #F0F0F0;"><br></span></p><p><span style="background-color: #F0F0F0;">.6667</span></p><p style="line-height: 0.5em;"><span style="background-color: #F0F0F0;"><br></span></p><p><span style="background-color: #F0F0F0;">0.666</span></p><p style="line-height: 0.5em;"><span style="background-color: #F0F0F0;"><br></span></p><p><span style="background-color: #F0F0F0;">0.667&nbsp;</span></p><p><span style="background-color: #F0F0F0;"><br></span></p></td>
+        <td width="160" valign="top" style="word-break: break-all;" align="center"><p><span style="background-color: #F0F0F0;"><br></span></p><p><span style="background-color: #F0F0F0;"><br></span></p><p><span style="background-color: #F0F0F0;">0.66</span></p><p style="line-height: 0.5em;"><span style="background-color: #F0F0F0;"><br></span></p><p><span style="background-color: #F0F0F0;">.66</span></p><p style="line-height: 0.5em;"><span style="background-color: #F0F0F0;"><br></span></p><p><span style="background-color: #F0F0F0;">0.67</span></p><p style="line-height: 0.5em;"><br></p><p><span style="background-color: #F0F0F0;">.67</span><br></p></td>
+      </tr>
+      <tr>
+        <td width="160" valign="top" align="center" style="word-break: break-all;"><p><br></p><p><br></p><p><img src="https://r.latexeasy.com/image.svg?-%5Cfrac%20%7B1%7D%20%7B3%7D" data-formula-image="-%5Cfrac%20%7B1%7D%20%7B3%7D"></p></td>
+        <td width="160" valign="top" style="word-break: break-all;" align="center"><p><span style="background-color: #F0F0F0;"><br></span></p><p><span style="background-color: #F0F0F0;">-1/3</span></p><p style="line-height: 0.5em;"><br></p><p><span style="background-color: #F0F0F0;">-.3333</span></p><p style="line-height: 0.5em;"><span style="background-color: #F0F0F0;"><br></span></p><p><span style="background-color: #F0F0F0;">-0.333&nbsp;</span></p><p><span style="background-color: #F0F0F0;"><br></span></p></td>
+        <td width="160" valign="top" style="word-break: break-all;" align="center"><p style="margin-top: 0px; margin-bottom: 0px; padding: 0px; line-height: 0.5em;"><br></p><p><span style="background-color: #F0F0F0;"><br></span></p><p><span style="background-color: #F0F0F0;"><br></span></p><p style="margin-top: 0px; margin-bottom: 0px; padding: 0px; line-height: 0.5em;"><span style="background-color: #F0F0F0;">-.33</span><br></p><p><br></p><p><span style="background-color: #F0F0F0;">-0.33</span><span style="background-color: #F0F0F0;"></span><br></p></td>
+      </tr>
+    </tbody>
+  </table>
+  <p><br></p>
+</div>
+  `.trim().replace(/\s*\n\s*/g, '')
 };
